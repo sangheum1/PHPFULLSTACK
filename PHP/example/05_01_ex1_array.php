4271f55b6551e50387cbc44b81762aaaec1e27ca
sangheum1 <js040141@naver.com>
2023-03-27 18:11:27 +0900

    0327

--- PHP/example/05_01_ex1_array.php
+++ PHP/example/05_01_ex1_array.php
@@ -19,42 +19,74 @@
 
 // 연상 배열
 $arr_ass = array("key1" => "val1", "key2" => "val2");
-echo $arr_ass["key1"];
+// echo $arr_ass["key1"];
 
 // 다차원 배열
-$arr_temp = array(
-    array(1,2,3,4)
-    , array(5,6,7,8)
-    , array(
-        array(9,10,11)
-    )
-);
+// $arr_temp = array(
+//     array(1,2,3,4)
+//     , array(5,6,7,8)
+//     , array(
+//         array(9,10,11)
+//     )
+// );
 // echo $arr_temp[2][0][1];
 
-$arr_temp_3 = $arr_temp[2];
+// $arr_temp_3 = $arr_temp[2];
 
-$arr_temp_3_c = array(
-        array(9, 10, 11)
-    );
+// $arr_temp_3_c = array(
+//         array(9, 10, 11)
+//     );
 
 // 배열의 원소 삭제
 
-$arr_week = array("sun", "mon", "delete", "tue", "wed");
-unset($arr_week[2]);
-print_r($arr_week);
+// $arr_week = array("sun", "mon", "delete", "tue", "wed");
+// unset($arr_week[2]);
+// print_r($arr_week);
 
-$arr_week = array("sun", "mon", "delete", "tue", "wed");
-unset($arr_week[2]);
-print_r($arr_week);
-$arr_week = array("sun", "mon", "delete", "tue", "wed");
-unset($arr_week[2]);
-print_r($arr_week);
-$arr_week = array("sun", "mon", "delete", "tue", "wed");
-unset($arr_week[2]);
-print_r($arr_week);
-$arr_week = array("sun", "mon", "delete", "tue", "wed");
-unset($arr_week[2]);
-print_r($arr_week);
+// 중복되지 않는 원소를 반환 array_diff()
+
+// $arr_diff_1 = array("b", "a", "c", "d");
+// $arr_diff_2 = array("a", "b", "d");
+// $arr_diff = array_diff($arr_diff_1, $arr_diff_2);
+// print_r($arr_diff);
+
+// 배열의 정렬 : asort(), arsort(), ksort(), krsort()
+
+// asort 오름차순 정렬, arsort 내림차순 정렬, ksort 키를 기준으로 오름차순 정렬, krsort 키를 기준으로 내림차순 정렬
+// asort($arr_diff_1);
+// print_r($arr_diff_1);
+
+// arsort($arr_diff_1);
+// print_r($arr_diff_1);
+
+// $arr_ksort = array("key1" => "val1"
+//                 , "key2" => "val2"
+//                 , "key4" => "val4"
+//                 , "key3" => "val3");
+
+// ksort($arr_ksort);
+// print_r($arr_ksort);
+
+// array의 길이(사이즈)를 반환하는 함수 : count()
+// echo count($arr_ksort);
+
+// foreach($array as $key => $val){}      =>key랑 value값을 다 출력
+// foreach($array as $val){}              =>value값만 출력
+$arr1 = array("key1" => "val1"
+                , "key2" => "val2"
+                , "key4" => "val4"
+                , "key3" => "val3");
+
+// foreach($arr1 as $key => $val) {
+//     echo $key." : ".$val."\n";
+// }
+
+foreach($arr1 as $val) {
+    echo $val."\n";
+}
+
+
+
 
 
 ?>
--- PHP/training/05_01_tng_array.php
+++ PHP/training/05_01_tng_array.php
@@ -13,18 +13,43 @@
 // 배열길이=4
 // $arr_ass = array(5>"밀가루", "볶음밥"=>"쌀", "탕수육"=>"고기", "붕어빵"=>"팥");
 
-$arr_ass_de1 = array("된장찌개" => "파"
-                    , "볶음밥" => "양파"
-                    , "김치" => "마늘"
-                    , "비빔밥" => "참기름"
-                );
+// $arr_ass_de1 = array("된장찌개" => "파"
+//                     , "볶음밥" => "양파"
+//                     , "김치" => "마늘"
+//                     , "비빔밥" => "참기름"
+//                 );
 
-unset($arr_ass_de1["김치"]);
+// unset($arr_ass_de1["김치"]);
 
-var_dump($arr_ass_de1);
+// var_dump($arr_ass_de1);
 
-print_r($arr_ass_de1);
+// print_r($arr_ass_de1);
 
+$arr_ass = array("짬뽕"=>"밀가루"
+                , "볶음밥"=>"쌀"
+                , "삭제"=>"값값"
+                , "탕수육"=>"고기"
+                , "붕어빵"=>"팥");
+
+// foreach문을 이용하여 키가 삭제인 요소를 제거해 주세요
+foreach($arr_ass as $key => $val){
+    if( $key === "짬뽕" ) {
+        echo $key." : ".$val;
+    }
+    else if ($key === "볶음밥") {
+        echo $key." : ".$val;
+    }
+    else if ($key === "삭제") {
+        unset($arr_ass[$key]);
+    }
+    else if ($key === "탕수육") {
+        echo $key." : ".$val;
+    }
+    else {
+        echo $key." : ".$val;
+    }
+}
+var_dump($arr_ass);
 
 
 
