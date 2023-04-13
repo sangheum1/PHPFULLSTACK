ba14bbf60fd0967ea59452819c156d2ddfa314d7
sangheum1 <js040141@naver.com>
2023-04-13 18:15:27 +0900

    0413

---
+++ PHP/training/board_test/db_common.php
@@ -0,0 +1,28 @@
+<?php
+function db_conn()
+{
+    $host = "localhost";
+    $user = "root";
+    $pass = "root506";
+    $charset = "utf8mb4";
+    $db_name = "boarder";
+    $dns = "mysql:host=".$host.";dbname=".$db_name.";charset=".$charset;
+    $pdo_option =
+        array(
+            PDO::ATTR_EMULATE_PREPARES      => false
+            ,PDO::ATTR_ERRMODE              => PDO::ERRMODE_EXCEPTION
+            ,PDO::ATTR_DEFAULT_FETCH_MODE   => PDO::FETCH_ASSOC
+        );
+    
+    $pdo = new PDO($dns, $user, $pass, $pdo_option);
+    $stmt = $pdo->prepare( " select * from board_info " );
+    $stmt->execute();
+    $result = $stmt->fetch_assoc();
+    var_dump($result);
+}
+
+db_conn();
+
+
+
+?>
